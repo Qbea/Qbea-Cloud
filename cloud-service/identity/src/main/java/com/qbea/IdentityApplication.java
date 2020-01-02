413fe3c56dc50403989505aec3b406b858e7a8fa
radish <35636529@qq.com>
2020-01-02 11:14:19 +0800

    微服务-用户中心框架提交

---
+++ cloud-service/identity/src/main/java/com/qbea/IdentityApplication.java
@@ -0,0 +1,13 @@
+package com.qbea;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * 　@author radishT
+ * 　@date: 2020/1/2 11:09
+ *
+ */
+@SpringBootApplication
+public class IdentityApplication {
+}
